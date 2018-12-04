04fe6135ab918a94bdf7483620b23ff6f29dc239
Erfaan <36810625+ErfaanAli@users.noreply.github.com>
2018-12-04 11:32:44 +0500

    Add files via upload

---
+++ Studentindex.php
@@ -0,0 +1,44 @@
+<html>
+<header><title>This is title</title></header>
+<body>
+  <link rel="stylesheet" type="text/css" href="Css/style.css">
+<ul class="navList">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="CourseRegistraion.html">Course Registration</a></li>
+        <li><a href="Attendance.html">Attendance</a></li>
+        <li><a href="Marks.html">Marks</a></li>
+        <li><a href="Transcript.html">Transcript</a></li>
+        <li><a href="FeeChalan.html">Fee Challan</a></li>
+        <li><a href="FeeDetail.html">Fee Details</a></li>
+      </ul>
+      
+      <div class="content">
+      
+   
+        
+        <h1 class="headings">University Information</h1>
+      
+
+
+        
+        <p>Student ID   sql query</p>
+        
+        <p> Degree:</p>
+        <p>Batch:</p>
+        <p>GPA:</p>
+        <p>Campus:</p>
+
+        <h1 class="headings">Personal Information</h1>
+        <p>Name :</p>
+        <p>Father Name :</p>
+        <p> Gender :</p>
+        <p>Email :</p>           
+    
+        <h1 class="headings">Contact Information</h1>
+        <p>Street :</p>
+        <p>City :</p>
+        <p>Country :</p>
+        
+      </div>
+    </body>
+    </html>
